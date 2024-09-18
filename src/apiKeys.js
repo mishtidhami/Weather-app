251c48f5452afc4c63894d5ab18a91df2eaa8111
Sapna <165400558+mishtidhami@users.noreply.github.com>
2024-09-18 11:51:06 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "30df6e9174c2bd1fed7e14035f2b93b4",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
